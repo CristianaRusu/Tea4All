b1b3a4539b48f361f51c0081024adce5e311778d
CristianaRusu <150471612+CristianaRusu@users.noreply.github.com>
2024-05-13 15:16:47 +0300

    13.05.2024 first draft

---
+++ src/main/java/com/entityClass/Category.java
@@ -0,0 +1,43 @@
+package com.entityClass;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorys")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", length = 45)
+    private String name;
+
+    @Column(name = "description", length = 1000)
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
