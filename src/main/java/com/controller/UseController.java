89958d210e3fcaac3708f1b0f3d135d6c4300e03
CristianaRusu <150471612+CristianaRusu@users.noreply.github.com>
2024-05-13 15:14:49 +0300

    13.05.2024 first draft

---
+++ src/main/java/com/controller/UseController.java
@@ -0,0 +1,32 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+import com.service.UserService;
+import com.entityClass.User;
+
+
+public class UseController {
+    @FXML
+    public ListView<User> userListView;
+
+    @FXML
+    void initialize() {
+        UserService userService = new UserService();
+        List<User> allUsers = userService.getAllUsers();
+
+        System.out.println(allUsers);
+        userListView.setItems(FXCollections.observableArrayList(new ArrayList<User>(allUsers)));
+
+        //test login
+        try{
+            System.out.println(userService.findUser("test", "1234").toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
